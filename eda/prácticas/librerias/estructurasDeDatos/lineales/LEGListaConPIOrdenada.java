c2d92598d14362cbaaead3fa3bdcefaf3ff68f47
irmumus <48769180+irmumus@users.noreply.github.com>
2020-04-20 19:19:57 +0200

    Update LEGListaConPIOrdenada.java

--- eda/prácticas/librerias/estructurasDeDatos/lineales/LEGListaConPIOrdenada.java
+++ eda/prácticas/librerias/estructurasDeDatos/lineales/LEGListaConPIOrdenada.java
@@ -1,1 +1,22 @@
+package librerias.estructurasDeDatos.lineales;
+import librerias.estructurasDeDatos.modelos.*;
 
+
+/**
+ * Write a description of class LEGListaConPIOrdenada here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LEGListaConPIOrdenada<E extends Comparable> 
+        extends LEGListaConPI<E> implements ListaConPI<E>{
+    
+    public void insertar(E e) { 
+        //metodos heredados de LEGListaConPI
+        inicio();
+        while(!esFin() && recuperar().compareTo(e)<0){
+            siguiente();
+        }
+        super.insertar(e);
+    }
+}
