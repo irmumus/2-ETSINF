f598aac75f1b28386efde05399d04967cac8be79
irmumus <48769180+irmumus@users.noreply.github.com>
2020-04-20 19:22:43 +0200

    Update EntradaHash.java

--- eda/prácticas/librerias/estructurasDeDatos/deDispersion/EntradaHash.java
+++ eda/prácticas/librerias/estructurasDeDatos/deDispersion/EntradaHash.java
@@ -1,1 +1,28 @@
+package librerias.estructurasDeDatos.deDispersion;
 
+/**
+ * Representa un elemento de la Lista con PI que implementa   
+ * una cubeta de una TablaHash, esto es una Entrada o par
+ * (clave, valor)
+ * 
+ * @param <C>, el tipo de la clave
+ * @param <V>, el tipo del valor asociado a una clave
+ * 
+ * @author (EDA)
+ * @version (Curso 2019-2020)
+ */
+
+class EntradaHash<C, V> {
+    
+    protected C clave;
+    protected V valor;
+
+    EntradaHash(C c, V v) {
+        clave = c;
+        valor = v;
+    }
+    
+    public String toString() { 
+        return "(" + clave + ", " + valor + ")"; 
+    }
+}
