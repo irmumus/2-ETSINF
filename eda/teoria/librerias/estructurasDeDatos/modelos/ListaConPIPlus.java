55a2edd3512090b9845534e78fe9299dcc9a80ea
irmumus <48769180+irmumus@users.noreply.github.com>
2020-06-11 15:30:08 +0200

    Update ListaConPIPlus.java

--- eda/teoria/librerias/estructurasDeDatos/modelos/ListaConPIPlus.java
+++ eda/teoria/librerias/estructurasDeDatos/modelos/ListaConPIPlus.java
@@ -4,4 +4,14 @@ import librerias.excepciones.*;
 
 public interface ListaConPIPlus<E> extends ListaConPI<E> {
     public boolean eliminarUltimo(E e);
+    
+    public boolean eliminarPrimero(E e);
+    
+    public boolean contiene(E e);
+    
+    public boolean eliminar(E e);
+    
+    public boolean eliminarTodos(E e);
+    
+    public void concatenar(ListaConPI<E> e);
 }
